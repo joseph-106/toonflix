de846ba5633020614b2b0d98fc7707db0693d215
joseph-106 <sjjoseph106@gmail.com>
2023-01-27 14:42:52 +0900

    x

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,8 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        backgroundColor: const Color(0xFFE7626C),
-        textTheme: const TextTheme(
-          headline1: TextStyle(
-            color: Color(0xFF232B55),
-          ),
-        ),
-        cardColor: const Color(0xFFF4EDDB),
-      ),
-      home: const HomeScreen(),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
