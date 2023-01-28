63fb360f19ee4baee50a090233eb36ea6fb6bd80
joseph-106 <sjjoseph106@gmail.com>
2023-01-28 14:58:35 +0900

    fromJson

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,15 +1,27 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
+
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      final webtoons = jsonDecode(response.body);
+
+      for (var webtoon in webtoons) {
+        final instance = WebtoonModel.fromJson(webtoon);
+
+        webtoonInstances.add(instance);
+      }
+
+      return webtoonInstances;
     }
     throw Error();
   }
